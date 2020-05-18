09d144b92f797f9f29ed8c984386107b6d43fbd1
Bikashbhaila <bikash.bhaila@outlook.com>
2020-05-18 16:38:22 -0500

    connect mogodb

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//For connecting MongoDB
+const mongoose = require("mongoose");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks");
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
